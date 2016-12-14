acc1bd72daa3c603713dcf0ac4322570f927d0f7
Logan Rogers <logan.s.rogers@biola.edu>
2016-12-14 13:26:41 -0800

    Navbar component made, still needs styling

---
+++ app/components/navbar/navbar.js
@@ -0,0 +1,81 @@
+'use strict';
+
+require('./_navbar.scss');
+
+module.exports = {
+  template: require('./navbar.html'),
+  controller: ['$log', '$location', '$rootScope', '$window', 'authService', NavbarController],
+  controllerAs: 'navbarCtrl',
+  bindings: {
+    appTitle: '@',
+  },
+};
+
+function NavbarController($log, $location, $rootScope, $window, authService) {
+  $log.debug('init navbarCtrl');
+
+  function pageLoadHandler() {
+    // if there is allready a token go the home page
+    console.log('pageLoadHandler is running');
+    let path = $location.path();
+
+    if (path !== '/#') {
+      this.hideLogout = false;
+      this.hideLogin = true;
+    }
+
+    if (path === '/#' ) {
+      this.hideLogout = true;
+      this.hideLogin = false;
+    }
+
+    if (path === '/#'){
+      this.hideLogout = true;
+      this.hideLogin = true;
+    }
+
+
+    authService.getToken()
+      .then(() => {
+        if(path === '/#' || path === '/login'){
+          $location.url('/#');
+        }
+        this.hideLogin = true;
+        this.hideLogout = false;
+      })
+      .catch(() => {
+        let query = $location.search();
+        if (query.token) {
+          return authService.setToken(query.token)
+            .then(() => {
+              $location.url('/profile');
+              this.hideLogin = true;
+              this.hideLogout = false;
+            });
+        }
+        if (path !== '/login' ){
+          $location.url('/landing');
+        }
+      });
+  }
+
+  $window.onload = pageLoadHandler.bind(this);
+  $rootScope.$on('$stateChangeStart', pageLoadHandler.bind(this));
+
+  this.login = function() {
+    this.hideLogout = true;
+    this.hideLogin = true;
+
+    $location.url('/login');
+  };
+
+  this.logout = function() {
+    $log.log('navbarCtrl.logout()');
+    this.hideLogout = true;
+    this.hideLogin = false;
+    authService.logout()
+      .then(() => {
+        $location.url('/');
+      });
+  };
+}
