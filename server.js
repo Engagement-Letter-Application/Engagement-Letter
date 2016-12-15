b2aa33e1b39f78c9bd77a9437844c238e035708b
brandon parker <brandon@dkparker.com>
2016-12-15 13:40:41 -0800

    auth basic setup, may not implement this version

--- server.js
+++ server.js
@@ -2,4 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const authRouter = require(__dirname + '/routes/auth_router');
+
+app.use('/api', authRouter);
+
 module.exports = app.listen(PORT, () => console.log('server up on port:' + PORT));
