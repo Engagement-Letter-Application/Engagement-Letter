86c6b4ce110f807aaeaf997abdfb44960bd777a4
brandon parker <brandon@dkparker.com>
2016-12-15 13:59:38 -0800

    added app secret, but needing to change promise library for auth

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
-
 const authRouter = require(__dirname + '/routes/auth_router');
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/engagement_db');
 
 app.use('/api', authRouter);
 
