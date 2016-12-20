268811d907685c752827bc71fead7d49df945824
brandon parker <brandon@dkparker.com>
2016-12-20 13:23:08 -0800

    Added setup for views

---
+++ app/view/auth/auth_controller.js
@@ -0,0 +1,20 @@
+'use strict';
+
+require('./_login.scss');
+
+module.exports = ['$log', '$rootScope', '$location', 'authService',  LoginController ];
+
+function LoginController($log, $rootScope, $location, authService){
+  $log.debug('init LoginController');
+
+  let query = $location.search();
+  console.log('query', query.token);
+
+  this.showSignin = true;
+
+  if(query.token){
+    authService.setToken(query.token)
+    .then(() => {
+      $location.path('/dashboard');
+    });
+  }
